dfeca126b6784310200d070e851edf7110ee141f
Cheshire-Katze <serj.older@gmail.com>
2020-04-21 10:00:38 +0300

    5

---
+++ 2-18_04_20/5/5.js
@@ -0,0 +1,26 @@
+let result;
+
+function sum(a,b) {
+    result = a+b;
+    return result;
+}
+
+function subtract(a,b) {
+    result = a-b;
+    return result;
+}
+
+function multiply(a,b) {
+    result = a*b;
+    return result;
+}
+
+function divide(a,b) {
+    result = a/b;
+    return result;
+}
+
+alert(sum(6,2))
+alert(subtract(6,2));
+alert(multiply(6,2));
+alert(divide(6,2));
