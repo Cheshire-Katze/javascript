3ca1664fe0e16e8fcfd6c467bf83b2c2693c2f2e
Cheshire-Katze <serj.older@gmail.com>
2020-04-24 20:11:08 +0300

    4 succ/

---
+++ 3-22_04_20/4/4.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let i;
+let s;
+
+for (i=1; i<=21; i++) {
+    for (s=0; s<i; s++) {
+        document.write(s);
+    }
+    document.write('<br>');
+
+}
