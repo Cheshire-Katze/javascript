dacb025cc5de31e3c575a1671c09e5ae381a1e0e
Cheshire-Katze <serj.older@gmail.com>
2020-04-26 19:29:53 +0300

    1,1

--- 4-25_04_20/1/1.js
+++ 4-25_04_20/1/1.js
@@ -1,12 +1,12 @@
 
 function number_in_object(a) {
-
-if (a>=0 && a<=999) {
     let number = {
         hundreds: 0,
         decades: 0,
         units: 0
     }
+if (a>=0 && a<=999) {
+
     number.hundreds = Math.floor(a/100);
     a = a-(number.hundreds*100);
     number.decades = Math.floor(a/10);
@@ -16,15 +16,30 @@ if (a>=0 && a<=999) {
     //return console.log('сотни: ' + number.hundreds + ' десятки: ' + number.decades + ' единицы: ' + number.units);
 
 } else if (a>999) {
-    return console.log('Введено некорректное число!');
+    delete number.hundreds;
+    delete number.decades;
+    delete number.units;
+    console.log('Введено некорректное число!');
+    return console.log(number);
 } else {
-    return console.log('Введены некорректные значения!');
+    delete number.hundreds;
+    delete number.decades;
+    delete number.units;
+    console.log('Введены некорректные значения!');
+    return console.log(number);
 }
 }
 
-number_in_object(245);
+number_in_object(prompt('Введите число от 0 до 999'));
+
+
 /*
 Вопросы:
+
 1) Как правильно выводить объект? 'return console.log(number);' или каждое свойство отдельно?
-2) Что значит "вывести пустой объект"? Можно пример. В google информацию не нашел.
+Сделал два вывода, на всякий случай.
+
+2) Что значит "вывести пустой объект"? Удалить все его свойства или вывести объект с нулевыми свойствами, без их удаления?
+Можно пример. В google информацию не нашел. Постарался сделать максимально похожий вывод.
+Но скорей всего не правильно, но ничего другого в голову не приходит.
 */
