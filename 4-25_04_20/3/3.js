a641f555bf12141d863dad40a8f469586d7a87ad
Cheshire-Katze <serj.older@gmail.com>
2020-04-27 13:35:43 +0300

    3_подсказки

--- 4-25_04_20/3/3.js
+++ 4-25_04_20/3/3.js
@@ -5,77 +5,62 @@ let questions = {
     3: '3-й вопрос'
 }
 let answers = {
-    1.1:'ответ 1',
-    1.2:'ответ 2',
-    1.3:'ответ 3',
-    1.4:'ответ 4',
+    11:'ответ 11',
+    12:'ответ 12',
+    13:'ответ 13',
+    14:'ответ 14',
 
-    2.1:'ответ 1',
-    2.2:'ответ 2',
-    2.3:'ответ 3',
-    2.4:'ответ 4',
+    21:'ответ 21',
+    22:'ответ 22',
+    23:'ответ 23',
+    24:'ответ 24',
 
-    3.1:'ответ 1',
-    3.2:'ответ 2',
-    3.3:'ответ 3',
-    3.4:'ответ 4'
+    31:'ответ 31',
+    32:'ответ 32',
+    33:'ответ 33',
+    34:'ответ 34'
+}
+let cor_ans = {
+    1: 2,
+    2: 4,
+    3: 1
 }
 let i=1;
 let answer_user;
 let next;
 let prize=0;
-
-// задаем вопрос пользователю
+let cost_que=100;
+let num_ans = 10;
+// задаем вопросы пользователю
 do {
-    answer_user = prompt('Вопрос на 100 р.!' + '\n' + questions[i] + '\n' + ' ' + '\n' + answers[1.1] + '\n' + answers[1.2] + '\n' + answers[1.3] + '\n' + answers[1.4] + '\n' + '' + '\n' + '-1 - выход');
+    answer_user = prompt('Вопрос на ' + cost_que + ' р.!' + '\n' + questions[i] + '\n' + ' ' + '\n' + answers[num_ans+1] + '\n' + answers[num_ans+2] + '\n' + answers[num_ans+3] + '\n' + answers[num_ans+4] + '\n' + '' + '\n' + '-1 - Забрать выигрышь. ' + 'Подсказки: ' + '50/50');
 
 if (answer_user==-1) {
-    alert('Вы даже не попробовали(');
+    alert('Игра окончена! Ваш выигрышь: ' + prize + ' р.');
     break;
-} else if (answer_user != 2) {
-    alert('Ответ неверный. Вы проиграли. Ваш выигрышь: ' + prize)
+} else if (answer_user != cor_ans[i]) {
+    prize=0;
+    alert('Ответ неверный. Вы проиграли. Ваш выигрышь: ' + prize + ' р.')
     break;
 } else {
-    prize = 100;
-    alert('Ответ правильный! Вы выиграли 100 р.!');
+    prize += 100;
+    alert('Ответ правильный! Вы выиграли ' + prize + ' р.!');
+    if (i<Object.keys(questions).length) {
     next = prompt('Хотите продолжить или заберете выигрышь?' + '\n' + '' + '\n' + '1 - Продолжить' + '\n' + '2 - Забрать выйгрышь');
     if (next == 2) {
-        alert('Поздравляю! Ваш выигрышь - ' + prize)
+        alert('Поздравляю! Ваш выигрышь - ' + prize + ' р.')
         break;
-    } else i++;
+    } else {
+        cost_que += 100;
+        i++;
+        num_ans +=10;
+    }
+    } else {
+        i++;
+        alert('Игра окончена! Ваш выигрышь: ' + prize + ' р.');
+    }
 }
-} while (i<=Object.keys(questions).length)
+} while (i<=Object.keys(questions).length);
 
 
 
-/*answer_user = prompt('Вопрос на 200 р.!' + '\n' + questions[i] + '\n' + ' ' + '\n' + answers[2.1] + '\n' + answers[2.2] + '\n' + answers[2.3] + '\n' + answers[2.4] + '\n' + '' + '\n' + '-1 - выход');
-
-if (answer_user==-1) {
-    alert('Поздравляю! Ваш выигрышь - ' + prize);
-    break;
-} else if (answer_user != 4) {
-    prize=0;
-    alert('Ответ неверный. Вы проиграли. Ваш выигрышь: ' + prize)
-    break;
-} else {
-    prize *= 2;
-    alert('Ответ правильный! Вы выиграли 200 р.!');
-    next = prompt('Хотите продолжить или заберете выигрышь?' + '\n' + '' + '\n' + '1 - Продолжить' + '\n' + '2 - Забрать выйгрышь');
-    if (next==2) {
-        alert('Поздравляю! Ваш выигрышь - ' + prize)
-        break;
-    } else i++;
-}*/
-
-
-
-
-
-
-
-
-
-
-function ask() {
-
-}
