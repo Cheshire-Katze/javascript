707d239eae57e31277df18e9c37af7c60fcbed82
Cheshire-Katze <serj.older@gmail.com>
2020-04-27 12:30:19 +0300

    3...

--- 4-25_04_20/3/3.js
+++ 4-25_04_20/3/3.js
@@ -22,14 +22,50 @@ let answers = {
 }
 let i=1;
 let answer_user;
+let next;
+let prize=0;
 
 // задаем вопрос пользователю
 do {
-    answer_user = prompt(questions[i] + '\n' + ' ' + '\n' + answers[1.1] + '\n' + answers[1.2] + '\n' + answers[1.3] + '\n' + answers[1.4]);
+    answer_user = prompt('Вопрос на 100 р.!' + '\n' + questions[i] + '\n' + ' ' + '\n' + answers[1.1] + '\n' + answers[1.2] + '\n' + answers[1.3] + '\n' + answers[1.4] + '\n' + '' + '\n' + '-1 - выход');
 
+if (answer_user==-1) {
+    alert('Вы даже не попробовали(');
+    break;
+} else if (answer_user != 2) {
+    alert('Ответ неверный. Вы проиграли. Ваш выигрышь: ' + prize)
+    break;
+} else {
+    prize = 100;
+    alert('Ответ правильный! Вы выиграли 100 р.!');
+    next = prompt('Хотите продолжить или заберете выигрышь?' + '\n' + '' + '\n' + '1 - Продолжить' + '\n' + '2 - Забрать выйгрышь');
+    if (next == 2) {
+        alert('Поздравляю! Ваш выигрышь - ' + prize)
+        break;
+    } else i++;
+}
 } while (i<=Object.keys(questions).length)
 
 
+
+/*answer_user = prompt('Вопрос на 200 р.!' + '\n' + questions[i] + '\n' + ' ' + '\n' + answers[2.1] + '\n' + answers[2.2] + '\n' + answers[2.3] + '\n' + answers[2.4] + '\n' + '' + '\n' + '-1 - выход');
+
+if (answer_user==-1) {
+    alert('Поздравляю! Ваш выигрышь - ' + prize);
+    break;
+} else if (answer_user != 4) {
+    prize=0;
+    alert('Ответ неверный. Вы проиграли. Ваш выигрышь: ' + prize)
+    break;
+} else {
+    prize *= 2;
+    alert('Ответ правильный! Вы выиграли 200 р.!');
+    next = prompt('Хотите продолжить или заберете выигрышь?' + '\n' + '' + '\n' + '1 - Продолжить' + '\n' + '2 - Забрать выйгрышь');
+    if (next==2) {
+        alert('Поздравляю! Ваш выигрышь - ' + prize)
+        break;
+    } else i++;
+}*/
 
 
 
